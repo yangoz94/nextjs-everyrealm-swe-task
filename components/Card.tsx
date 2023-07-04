e7928841654c4c2a336dd1bf62601a4d8f75e347
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-04 15:15:59 -0400

    remove redundant css attrs

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
+//props.children is also a good option for card components in general but since the props are specified in the instructions, I sticked to method below
 type CardProps = {
-  // if we want to make any of them optional, we can do so with ? and conditional rendering in the jsx
   image: string;
   title: string;
   description: string;
@@ -17,15 +17,13 @@ export default function Card({
 }: CardProps) {
   return (
     <div
-      className={`mx-5 flex max-w-xl flex-col items-center justify-center gap-5 rounded-lg bg-PRIMARY_BG_COLOR p-5 px-5 shadow-lg ${className}`}
+      className={`mx-5 flex max-w-xl flex-col  items-center justify-center gap-5 rounded-lg bg-PRIMARY_BG_COLOR p-5 shadow-lg ${className}`}
     >
       <Image src={image} alt={title} width={200} height={200} />
-      <div className="grid gap-3 text-center">
+      <div className="grid gap-5 text-center">
         <h2 className="text-xl font-bold">{title}</h2>
-        <p className="text-base text-gray-700">{description}</p>
+        <p className="text-gray-700">{description}</p>
       </div>
     </div>
   );
 }
-
-//props.children is also a good option but since the props are specified in the instructions, I used the above method
