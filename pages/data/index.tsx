92a47a8c1e58f74b4962f16d0e9fbee534c97bcd
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-04 14:47:52 -0400

    create data/index.tsx with a list of products fetched

---
+++ pages/data/index.tsx
@@ -0,0 +1,31 @@
+import { Product } from "@/store/store";
+import Spinner from "@/components/Spinner";
+import { useProducts } from "@/hooks/useProducts";
+import BackButton from "@/components/BackButton";
+
+export default function Data() {
+  const { products, isLoading, isError } = useProducts();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <p>Something went wrong</p>;
+  }
+
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center gap-5 bg-yellow-200 p-5">
+      <BackButton href="/" style="absolute top-5 left-5" />
+      <h1 className="text-xl font-extrabold">Welcome to the data page!</h1>
+      <p>Below you can find all the data fetched with React Query;</p>
+      <ul className="grid gap-3">
+        {(products as Product[]).map((product) => (
+          <li key={product.id} className="before:pr-2 before:content-['✔️']">
+            {product.title} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
