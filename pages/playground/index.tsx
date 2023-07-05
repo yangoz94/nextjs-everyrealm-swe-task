4ee03214239750e4b77479f99c8971240f2b4429
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-04 20:23:45 -0400

    add Toast demo section, refactor existing sections

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,39 +1,85 @@
+import React, { useState } from "react";
 import Card from "@/components/Card";
 import BackButton from "@/components/BackButton";
-import { useState } from "react";
 import Modal from "@/components/Modal";
+import Toast from "@/components/Toast";
+import Spinner from "@/components/Spinner";
+import Button from "@/components/Button"; // import the button component
 
 export default function Playground() {
+  // I would consider useReducer after more than 2 local states if this was a real project
   const [showModal, setShowModal] = useState(false);
+  const [toastType, setToastType] = useState< "info" | "success" | "warning" | "error" >("info");
+  const [isToastVisible, setIsToastVisible] = useState(false);
+  const [isSpinnerDisplayed, setIsSpinnerDisplayed] = useState(false);
 
-  // create a function to toggle the modal visibility
   const toggleModal = () => {
-    setShowModal((prev: boolean) => !prev);
+    setShowModal((prev) => !prev);
+  };
+
+  const showToast = (type: "info" | "success" | "warning" | "error") => {
+    setToastType(type);
+    setIsToastVisible(true);
+  };
+
+  const handleToastClose = () => {
+    setIsToastVisible(false);
+  };
+
+  const displaySpinner = () => {
+    setIsSpinnerDisplayed(true);
+    setTimeout(() => {
+      setIsSpinnerDisplayed(false);
+    }, 3000);
   };
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center gap-5 bg-red-200 py-5">
-      <BackButton href="/" className="absolute left-5 top-5" />
-      <h1 className="extrabold text-2xl">Playground for Components</h1>
+    <main className="relative flex min-h-screen flex-col items-center gap-5 py-5">
+      <section id="title-navigation">
+        <BackButton href="/" className="absolute left-5 top-5" />
+        <h1 className="extrabold text-2xl">Playground for Components</h1>
+      </section>
+
       {/* Card demo */}
-      <Card
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
-        title="Sample Card Component"
-        image="/next.svg"
-      />
+      <section id="card-demo">
+        <Card
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+          title="Sample Card Component"
+          image="/next.svg" //optional
+        />
+      </section>
+
       {/* Modal demo */}
-      <button
-        className="rounded bg-black px-4 py-2 text-white duration-300 hover:bg-white hover:text-black"
-        onClick={toggleModal}
-      >
-        Open Modal
-      </button>
-      <Modal isOpen={showModal} onClose={toggleModal}>
-        <div className="grid gap-3 px-3">
-          <h2 className="text-xl font-bold">Hey, I am a Modal Component!</h2>
-          <p>Insert some cool content in here</p>
-        </div>
-      </Modal>
+      <section id="modal-demo">
+        <Button onClick={toggleModal}>Open Modal</Button>
+        <Modal isOpen={showModal} onClose={toggleModal}>
+          <div className="grid gap-3 px-3">
+            <h2 className="text-xl font-bold">Hey, I am a Modal Component!</h2>
+            <p>Insert some cool content in here</p>
+          </div>
+        </Modal>
+      </section>
+
+      {/* Toast demo - Buttons can be mapped as well */}
+      <section className="mx-5 flex gap-3">
+        <Button onClick={() => showToast("info")}>Info Toast</Button>
+        <Button onClick={() => showToast("success")}>Success Toast</Button>
+        <Button onClick={() => showToast("warning")}>Warning Toast</Button>
+        <Button onClick={() => showToast("error")}>Error Toast</Button>
+        <Toast
+          type={toastType}
+          isOpen={isToastVisible}
+          onClose={handleToastClose}
+        />
+      </section>
+      {/* Spinner demo */}
+      <section id="spinner-demo">
+        {isSpinnerDisplayed ? (
+          <Spinner />
+        ) : (
+          <Button onClick={displaySpinner}>Show Spinner for 3 seconds</Button>
+        )}
+      </section>
     </main>
   );
 }
