a3323ebc2031434ba9ca425eb895454f851c0d1f
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-05 02:35:58 -0400

    add priority attr to next/image component

--- components/Card.tsx
+++ components/Card.tsx
@@ -19,7 +19,7 @@ export default function Card({
     <div
       className={`mx-5 flex max-w-xl flex-col items-center justify-center gap-5 rounded-lg bg-white p-5 shadow-lg ${className}`}
     >
-      {image && <Image src={image} alt={title} width={200} height={200} />}
+      {image && <Image src={image} alt={title} width={200} height={200} priority={true} />}
       <div className="grid gap-5 text-center">
         <h2 className="text-xl font-bold">{title}</h2>
         <p className="text-gray-700">{description}</p>
