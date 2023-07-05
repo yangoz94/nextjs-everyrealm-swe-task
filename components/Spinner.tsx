227d68f6b863bd403bd53879d52209379f4efd48
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-04 20:19:36 -0400

    add optional classname prop for customizability

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -7,7 +7,7 @@ type SpinnerProps = {
 export default function Spinner({ className }: SpinnerProps) {
   return (
     <div
-      className={`flex min-h-screen items-center justify-center ${className}`}
+      className={`flex items-center justify-center hover:cursor-not-allowed ${className}`}
     >
       <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
