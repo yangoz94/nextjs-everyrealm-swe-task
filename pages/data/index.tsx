2dcc5add82c5c78b37e1e032c2f1c03ac77c7808
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2023-07-04 20:22:39 -0400

    update css attrs of /data page

--- pages/data/index.tsx
+++ pages/data/index.tsx
@@ -7,7 +7,7 @@ export default function Data() {
   const { products, isLoading, isError } = useProducts();
 
   if (isLoading) {
-    return <Spinner />;
+    return <Spinner className="absolute inset-0.5" />;
   }
 
   if (isError) {
@@ -15,7 +15,7 @@ export default function Data() {
   }
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center gap-5 bg-yellow-200 p-5">
+    <main className="relative flex min-h-screen flex-col items-center justify-center gap-5 p-5">
       <BackButton href="/" className="absolute left-5 top-5" />
       <h1 className="text-xl font-extrabold">Welcome to the data page!</h1>
       <p>Below you can find all the data fetched with React Query;</p>
